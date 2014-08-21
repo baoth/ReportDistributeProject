68961c20a29441f49808c5984b6b119b03a5bf2d
hanyuxing5588 <707297069@qq.com>
2014-08-21 15:29:13 +0800

    add logon

---
+++ weixinreportviews/Controllers/LogonController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace weixinreportviews.Controllers
+{
+    public class LogonController : Controller
+    {
+        //三级1
+        public ActionResult Index()
+        {
+            return View();
+        }
+      
+    }
+}
